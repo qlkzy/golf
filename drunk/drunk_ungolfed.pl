233ae06c758a28434fc7fedf59422478811d8492
David Morris <davidrowlandmorris@gmail.com>
2016-02-14 14:14:04 +0000

    Save bytes

--- drunk/drunk_ungolfed.pl
+++ drunk/drunk_ungolfed.pl
@@ -1,12 +1,7 @@
 # char->number conversion table
 # declared using barewords except for 's', which can't be a bareword
 # because it's a keyword
-%u=(o,0.5, e,0.1, i,0.2, a,-0.1, g,2, b,5, s=>0.2);
-
-# function to replace negative numbers or non-number with '0'
-sub t {
-    $_[0] > 0 ? $_[0] : 0
-}
+%u=(o,.5, e,.1, i,.2, a,-.1, g,2, b,5, s=>.2);
 
 # extract the number, first letter of first word, second letter of
 # second word
@@ -19,4 +14,4 @@ $x += $1 * $u{$2} * $u{$3}
 }{
 
 # format output
-$_ = t $x . ' ' . t ($x-0.5)*10
+$_ = ($x>0?$x:0) . $" . ($x-0.5>0?$x-0.5:0)*10
